860c59c6802267e6a39bc061a8131062af99645b
sjaehn <kontakt@jahnichen.de>
2025-12-30 21:29:50 +0100

    Add mouse hover effect.

--- cbutton.py
+++ cbutton.py
@@ -1,6 +1,19 @@
-from kivy.properties import StringProperty
+from kivy.core.window import Window
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.button import Button
 
 
 class CButton(Button):
     name = StringProperty("")
+    hover = BooleanProperty(False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(mouse_pos=self.on_mouse_over)
+
+    def on_mouse_over(self, window, pos):
+        if self.collide_point(*pos):
+            self.hover = True
+        else:
+            self.hover = False
+
